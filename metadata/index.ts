059dbcd08afdf870a7cba071ff12258eab4939f3
tianhukj <166341634+tianhukj@users.noreply.github.com>
2025-12-03 20:51:28 +0800

    Update index.ts

--- metadata/index.ts
+++ metadata/index.ts
@@ -1,17 +1,17 @@
 /** 文本 */
-export const siteName = 'Norvinsk Stalker知识库'
-export const siteShortName = 'NorStlkNolebase'
-export const siteDescription = '《诺文斯克潜行者》知识库'
+export const siteName = '天互科技知识库'
+export const siteShortName = 'Tianhu'
+export const siteDescription = '天互科技知识库'
 
 /** 文档所在目录 */
 export const include = ['教程', '指南']
 
 /** Repo */
-export const githubRepoLink = 'https://github.com/windersvista/NorvinskStalker_knowledgeBase'
+export const githubRepoLink = 'https://github.com/tianhukj/knowledgeBase'
 /** Aifadian */
-export const discordLink = 'https://afdian.com/a/samuel_szeto'
+export const discordLink = 'https://afdian.com/a/tianhukj'
 
 /** 无协议前缀域名 */
-export const plainTargetDomain = 'https://norvinsk-stalker-knowledge-base.vercel.app'
+export const plainTargetDomain = 'kownledge.tianhu.dpdns.org'
 /** 完整域名 */
 export const targetDomain = `https://${plainTargetDomain}`
